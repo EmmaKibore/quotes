a36612bc06a87e43c1c8589b9d4d46070ed338f7
EmmaKibore <emmakibore@gmail.com>
2019-03-27 09:37:46 +0300

    added quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -15,11 +15,13 @@ export class QuoteComponent implements OnInit {
     new Quote(3,'Life is too short for long term grudges','By Amira princess',new Date(2019,6,6),0,0),
     new Quote(4,'Childhood is a short season','By Evance Kipruto',new Date(2019,10,10),0,0),
     new Quote(5,'Shortcuts make long delays','By Khadija Farid',new Date(2019,4,4),0,0),
-    new Quote(6,'Your perception of me is a reflection of you','By Emma KIbore',new Date(2019,5,5),0,0),
+    new Quote(6,'Your perception of me is a reflection of you','By Emma Kibore',new Date(2019,5,5),0,0),
     new Quote(7,'Keep your face to the sunshine and you cannot see a shadow','By Eddy Bombo',new Date(2019,11,11),0,0),
     new Quote(8,'Your positive action combined with positive thinking result in success.','By Hussein Django',new Date(2019,12,15),0,0)
+    new Quote(9,'Shortcuts make long delays','By Shirley Katumbo',new Date(2019,11,11),0,0),
+    new Quote(10,'Childhood is a short season','By Cherry Shah',new Date(2019,5,12),0,0),
   ]
-  
+
   deleteQuote(isComplete,index){
     if(isComplete){
       let toDelete=confirm('Are you sure you want to this quote')
